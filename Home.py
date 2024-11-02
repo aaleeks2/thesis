678e5cf8236a805ca16d7c062581abf300eea24c
Aleks <aleksanderlapinski@wp.pl>
2024-11-02 14:39:10 +0100

    Update Home page

--- Home.py
+++ Home.py
@@ -1,6 +1,5 @@
 import streamlit as st
-import thesis
-st.set_page_config(layout="wide")
-st.write('# Zastosowanie technik uczenia maszynowego w systemach rekomendacji filmów w oparciu o ich cechy lub oceny widzów')
-st.markdown('<h2 style="text-align: center;">Aleksander Łapiński - s22064</h2>', unsafe_allow_html=True)
 
+st.set_page_config(layout="wide")
+st.write('# Zastosowanie technik uczenia maszynowego w systemach rekomendacji filmów')
+st.markdown('<h2 style="text-align: center;">Aleksander Łapiński - s22064</h2>', unsafe_allow_html=True)
