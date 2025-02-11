9180fdca2027412dfa40080991a7ddeaab6bfa49
Aleks <aleksanderlapinski@wp.pl>
2025-02-11 20:58:34 +0100

    Add two new metrics to Models evaluation page

--- pages/3_Models_evaluation.py
+++ pages/3_Models_evaluation.py
@@ -11,12 +11,16 @@ collab_recommenders = collaborative.CollaborativeBasedRecommender()
 st.header('Singular Value Decomposition')
 collab_recommenders.decomposition_algo.set_algo(collaborative.AlgorithmType.SVD)
 validate_results_svd, rmse_svd = collab_recommenders.decomposition_algo.validate_model()
-svd_rmse, svd_mae, svd_fit_time, svd_test_time = st.columns(4)
+svd_rmse, svd_mae, svd_mse, svd_fcp, svd_fit_time, svd_test_time = st.columns(6)
 
 with svd_rmse:
     svd_rmse.metric('RMSE', round(rmse_svd, 3))
 with svd_mae:
     svd_mae.metric('MAE', round(statistics.mean(validate_results_svd.get('test_mae')), 3))
+with svd_mse:
+    svd_mse.metric('MSE', round(statistics.mean(validate_results_svd.get('test_mse')), 3))
+with svd_fcp:
+    svd_fcp.metric('FCP', round(statistics.mean(validate_results_svd.get('test_fcp')), 3))
 with svd_fit_time:
     svd_fit_time.metric('Fit time (mean)', round(statistics.mean(validate_results_svd.get('fit_time')), 4))
 with svd_test_time:
@@ -25,12 +29,16 @@ with svd_test_time:
 st.header('Non-negative Matrix Factorization')
 collab_recommenders.decomposition_algo.set_algo(collaborative.AlgorithmType.NMF)
 validate_results_nmf, rmse_nmf = collab_recommenders.decomposition_algo.validate_model()
-nmf_rmse, nmf_mae, nmf_fit_time, nmf_test_time = st.columns(4)
+nmf_rmse, nmf_mae, nmf_mse, nmf_fcp, nmf_fit_time, nmf_test_time = st.columns(6)
 
 with nmf_rmse:
     nmf_rmse.metric('RMSE', round(rmse_nmf, 3))
 with nmf_mae:
     nmf_mae.metric('MAE', round(statistics.mean(validate_results_nmf.get('test_mae')), 3))
+with nmf_mse:
+    nmf_mse.metric('MSE', round(statistics.mean(validate_results_nmf.get('test_mse')), 3))
+with nmf_fcp:
+    nmf_fcp.metric('FCP', round(statistics.mean(validate_results_nmf.get('test_fcp')), 3))
 with nmf_fit_time:
     nmf_fit_time.metric('Fit time (mean)', round(statistics.mean(validate_results_nmf.get('fit_time')), 4))
 with nmf_test_time:
